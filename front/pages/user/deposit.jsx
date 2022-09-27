aaddae2934c5ec55089eb0fe822070cad93807ef
Song-jaehong <4leaf.SJH@gmail.com>
2022-09-27 18:18:53 +0900

    a

--- front/pages/user/deposit.jsx
+++ front/pages/user/deposit.jsx
@@ -95,8 +95,8 @@ const Deposit = () => {
       "https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/willmarkets/assets/images/user/qr_eth.png",
       "ERC-20",
     ],
-    USTD: [
-      "USTD",
+    USDT: [
+      "USDT",
       "THpf2R15EgZD6J6oFjr3dVjrYNTVPxWzp6",
       "https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/willmarkets/assets/images/user/qr_ustd.png",
       "TRC-20",
